2fbf8952bfbe4d53958cf2a58fc1a6c2b1abcc08
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2022-07-04 12:14:11 +0700

    Update Convert.py

--- Convert.py
+++ Convert.py
@@ -63,7 +63,7 @@ class convert:
     else:
       masuk = input(f'''{HIJAU}        JANGAN KOSONG GOBLOK!!!
               {HIJAU}TEKAN {MERAH}ENTER!!{H}''');os.system('python Convert.py')
-     elif masuk == '0' or masuk == '00':
+     elif masuk == '0':
        print(f"Loading...");os.system('git pull')
   def __satu__(self,cookie):
     try:
