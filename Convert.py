1b79f5d3c27e472fd4b2fc3ddd3cf45313cb5236
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2022-07-06 21:10:10 +0700

    Update Convert.py

--- Convert.py
+++ Convert.py
@@ -45,7 +45,6 @@ class convert:
     elif masuk == '4' or masuk == '04':
       exit()
     else:
-      exit(f"{MERAH}!.{MERAH} Wrong input")
     sleep (1)
     os.system('git pull');convert()
   def __satu__(self,cookie):
